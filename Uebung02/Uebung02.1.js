cbc580f396073093d4a08aac6358168871241b87
FabianRuefenacht <f.ruefenacht@outlook.com>
2022-11-11 13:53:10 +0100

    Uebug07
    Übung 7
    Fabian Rüfenacht

--- Uebung02/Uebung02.1.js
+++ Uebung02/Uebung02.1.js
@@ -1,8 +1,8 @@
 "use strict";
 var L = []; // erstellt leere Liste
 
-for (var i = 1; i < 100; i += 2) {  // Variabelndefinition der for-Schleife
+for (let i = 1; i < 100; i += 2) {  // Variabelndefinition der for-Schleife
     L.push(i); // Objekte zu Array hinzufügen
 }
 
-console.log(L) // Ausgabe des Array um zu testen ob die Schleife funktioniert
+console.log(L); // Ausgabe des Array um zu testen ob die Schleife funktioniert
